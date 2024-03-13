bd5dc8000bba3ba88aafe3ff77c9d6825a0d075b
sofyasona <sonka9810@yandex.ru>
2024-03-13 10:27:24 +0300

    создали счетчик

--- Program.cs
+++ Program.cs
@@ -4,3 +4,12 @@ for (int i= 0; i < str.Length; i++)
     Console.Write($"Введите {i}элемент массива:");
     str[i] = Console.ReadLine()!;
 }
+int j = 0;
+for (int i= 0; i < str.Length; i++)
+{
+int Length = str[i].Length;
+if (Length<= 3)
+{
+    j++;
+}
+}
