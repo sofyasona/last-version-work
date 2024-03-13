f92b477b05bbb74a14e0aadd6ef1a198e9c2daf7
sofyasona <sonka9810@yandex.ru>
2024-03-13 10:24:04 +0300

    ввели массив

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string [] str = new string [new Random().Next(1,11)];
+for (int i= 0; i < str.Length; i++)
+{
+    Console.Write($"Введите {i}элемент массива:");
+    str[i] = Console.ReadLine()!;
+}
